903a9a771ebfa5e15d1f8f52214161883e763c76
gabicilfone <cilfonegabriel@gmail.com>
2023-03-08 19:07:09 -0300

    revision changes

--- mylist.rb
+++ mylist.rb
@@ -3,8 +3,8 @@ require_relative './my_enumerable'
 class MyList
   include MyEnumerable
 
-  def initialize(list)
-    @list = list
+  def initialize(*args)
+    @list = *args
   end
 
   def each(&block)
@@ -12,7 +12,7 @@ class MyList
   end
 end
 
-list = MyList.new([1, 2, 3, 4])
+list = MyList.new(1, 2, 3, 4)
 
 puts(list.all? { |e| e < 5 }) # true
 
