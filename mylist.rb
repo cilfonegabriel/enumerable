021a2c5023d420f30db9cbdf21a8d817db8e8a45
HunterX-7 <fraquin7@gmail.com>
2023-03-08 16:11:17 -0500

    Add test for methods

--- mylist.rb
+++ mylist.rb
@@ -12,4 +12,14 @@ class MyList
   end
 end
 
-list = MyList.new([1, 2, 3, 4]) # true
+list = MyList.new([1, 2, 3, 4])
+
+puts(list.all? { |e| e < 5 }) # true
+
+puts(list.all? { |e| e > 5 }) # false
+
+puts(list.any? { |e| e == 2 }) # true
+
+puts(list.any? { |e| e == 5 }) # false
+
+puts(list.filter(&:even?)) # 2, 4
