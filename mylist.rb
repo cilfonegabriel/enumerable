763b9e1d5ca29ecc00427b943cc6ac2fd11a7ed2
gabicilfone <cilfonegabriel@gmail.com>
2023-03-08 17:13:29 -0300

    add mylist.rb file

---
+++ mylist.rb
@@ -0,0 +1,16 @@
+require_relative './my_enumerable'
+
+class MyList
+
+    include MyEnumerable
+
+    def initialize(list)
+        @list = list
+    end
+
+    def each(&block)
+        @list.each(&block)
+    end
+end
+
+new_list = MyList.new([1, 2, 3, 4]) # true
